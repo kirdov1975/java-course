85220b3dafca70cb151d75c8423fe0dcbaef4e04
kirdov1975 <kirdov1975@gmail.com>
2016-03-23 22:12:13 +0100

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,9 @@
 public class Calculate {
         public static void main(String[] arg) {
-                System.out.println("Calculate...");      
+                System.out.println("Calculate...");  
+                int base = x;
+                int power = x ;    
+                System.out.println("Result: " + (int)Math.pow(base, power));
         }
 }
     
- 
